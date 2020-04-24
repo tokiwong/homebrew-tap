56f2e41ce980e887dcad4fa5a575dd4e11b13a81
tokiwong <alvin.wong@rtslabs.com>
2020-04-24 15:11:58 -0400

    adding helmswitch rb

---
+++ helm-switcher.rb
@@ -0,0 +1,23 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class HelmSwitcher < Formula
+  desc "CLI tool to help install and switch between versions of Helm for Kubernetes"
+  homepage ""
+  url "https://github.com/tokiwong/helm-switcher/releases/download/v0.0.1/helmswitch-darwin-amd64.zip"
+  sha256 "96c5a0e1a37e61855c60e530f2ac0e401537838b73cd8d8fa2b95417bd17843f"
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    # Remove unrecognized options if warned by configure
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    # system "cmake", ".", *std_cmake_args
+    bin.install "helmswitch"
+  end
+
+end
