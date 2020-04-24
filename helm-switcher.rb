13ebf5c53134ee8f2bb12fa4c9fbf399444bb8f8
tokiwong <alvin.wong@rtslabs.com>
2020-04-24 15:56:04 -0400

    0.0.2

--- helm-switcher.rb
+++ helm-switcher.rb
@@ -4,7 +4,7 @@
 class HelmSwitcher < Formula
   desc "CLI tool to help install and switch between versions of Helm for Kubernetes"
   homepage ""
-  url "https://github.com/tokiwong/helm-switcher/releases/download/v0.0.1/helmswitch-darwin-amd64.zip"
+  url "https://github.com/tokiwong/helm-switcher/releases/download/v0.0.2/helmswitch-darwin-amd64.zip"
   sha256 "96c5a0e1a37e61855c60e530f2ac0e401537838b73cd8d8fa2b95417bd17843f"
 
   def install
